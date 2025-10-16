cbdcfe4ec1f48a87485b567a75a2583c60aac7a5
cocham <mh020805@naver.com>
2025-10-16 13:50:59 +0900

    feat: Define message and number constants

---
+++ src/main/java/calculator/constant/MessageConst.java
@@ -0,0 +1,18 @@
+package calculator.constant;
+
+/*
+    메시지 관련 상수 관리 클래스
+ */
+public class MessageConst {
+    public static final String START_MSG = "덧셈할 문자열을 입력해 주세요.";
+    public static final String OUTPUT_MSG = "결과 : ";
+
+    public static final String INPUT_MINUS_EXCEPTION_MSG = "음수는 입력할 수 없습니다.";
+    public static final String INPUT_FORM_EXCEPTION_MSG = "구분자와 숫자 이외의 값은 입력할 수 없습니다.";
+    public static final String INPUT_SEQUENCE_EXCEPTION_MSG = "구분자는 연속해서 입력할 수 없습니다.";
+    public static final String INPUT_CUSTOM_EXCEPTION_MSG = "커스텀 구분자를 입력하세요.";
+    public static final String INPUT_CUSTOM_UNVALIDATE_EXCEPTION = "구분자에 숫자는 사용할 수 없습니다.";
+    public static final String INPUT_CUSTOM_END_EXCEPTION_MSG = "구분자 형식이 잘못 되었습니다.";
+    public static final String INPUT_CUSTOM_NAN_NUMS_EXCEPTION_MSG = "구분자만 입력하면 안됩니다.";
+
+}
---
+++ src/main/java/calculator/constant/NumberConst.java
@@ -0,0 +1,6 @@
+package calculator.constant;
+
+public class NumberConst {
+    public static Integer INPUT_ZERO = 0;
+    public static Integer INPUT_LENGTH = 3;
+}
