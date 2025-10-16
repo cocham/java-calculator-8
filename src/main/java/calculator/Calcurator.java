5b2b8b1304ddbb60d7e0f29c7facc53bd3f38f0f
cocham <mh020805@naver.com>
2025-10-17 00:39:24 +0900

    feat: 계산기 핵심 로직 및 전체 흐름 구현
    Calculator 클래스를 중심으로 전체 애플리케이션의 동작 흐름 구현.
    
    - 역할별로 분리된 유틸리티 클래스를 사용하여 `입력 -> 파싱 -> 계산 -> 출력` 순서로 로직 수행
    - `try-catch` 구문을 추가하여 유효성 검사 등에서 발생하는 `IllegalArgumentException`을 처리하고, 에러 메시지를 출력하도록 구현.

---
+++ src/main/java/calculator/Calcurator.java
@@ -0,0 +1,27 @@
+package calculator;
+import calculator.util.ComputerUtil;
+import calculator.util.InputParser;
+import calculator.util.MessageUtil;
+import calculator.util.UserUtil;
+import java.util.List;
+
+public class Calcurator {
+    private final UserUtil userUtil = new UserUtil();
+
+    private final MessageUtil messageUtil = new MessageUtil();
+
+    private final InputParser inputParser = new InputParser();
+
+    private final ComputerUtil computerUtil = new ComputerUtil();
+
+    public void run() {
+        try{
+            String input = messageUtil.requestInput();
+            List<Integer> nums = inputParser.parse(input);
+            long sum = computerUtil.getInputInt(nums);
+            messageUtil.printResult(sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
