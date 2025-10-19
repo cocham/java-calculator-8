1f1d99a35b3ab78a130564d1014b79a36fa04020
cocham <mh020805@naver.com>
2025-10-19 21:07:57 +0900

    Revert "fix: 커스텀 구분자 파싱 시 리터럴 "\\n"을 찾도록 수정"
    extractCustomDelimiter에서 indexOf가 '줄바꿈' 특수 문자("\n")를 찾고 있어,
    실제 입력값인 문자 조합("\\n")을 인식하지 못하는 문제를 해결했다.
    This reverts commit 67d8895963bb383431cf72ef9925fd87293cb33e.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,14 +17,6 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 커스텀_구분자_사용2() {
-        assertSimpleTest(() -> {
-            run("//;\\n3;2");
-            assertThat(output()).contains("결과 : 5");
-        });
-    }
-
-    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
