733d8eceb5a02b01225d25a79e2a907e2d995162
cocham <mh020805@naver.com>
2025-10-20 16:12:58 +0900

    refactor(Calcurator): 주석 제거

--- src/main/java/calculator/Calcurator.java
+++ src/main/java/calculator/Calcurator.java
@@ -14,18 +14,7 @@ public class Calcurator {
 
     private final ComputerUtil computerUtil = new ComputerUtil();
 
-//    public void run() {
-//        try{
-//            String input = messageUtil.requestInput();
-//            List<Integer> nums = inputParser.parse(input);
-//            long sum = computerUtil.getInputInt(nums);
-//            messageUtil.printResult(sum);
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-
-        public void run() {
+    public void run() {
             String input = messageUtil.requestInput();
             List<Integer> nums = inputParser.parse(input);
             int sum = computerUtil.getInputInt(nums);
